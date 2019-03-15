1ff2dca27fd240a8f99dff90750611c2b91322c8
Georgi Lyubenov <joro2404@abv.bg>
2019-03-15 11:52:41 +0200

    Input fields

--- sellers/vue.js
+++ sellers/vue.js
@@ -1,16 +1,12 @@
 new Vue({
   el: '#editor',
   data: {
-    input: '# hello'
+    input: 
   },
   computed: {
-    compiledMarkdown: function () {
-      return marked(this.input, { sanitize: true })
-    }
+
   },
   methods: {
-    update: _.debounce(function (e) {
-      this.input = e.target.value
-    }, 300)
+
   }
 })
