3e6de8fa51589b52542399874da03a06e72fc25e
Samuil Georgiev <sami.georgiev@gmail.com>
2019-03-15 18:44:55 +0200

    All funcs works

--- Contract/contracts/ProductChain.sol
+++ Contract/contracts/ProductChain.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.5.0;
 contract ProductChain {
 
     struct Company {
-        string name;
-        string origin;
+        bytes32 name;
+        bytes32 origin;
         address p_key;
     }
 
@@ -12,13 +12,14 @@ contract ProductChain {
         Company seller;
         Company buyer;
         uint datastamp;
-        uint8 id;
+        uint32 id;
     }
 
     struct Product {
-        string name;
-        string description;
+        bytes32 name;
+        bytes32 description;
         uint32 id;
+        uint32 transactions_count;
     }
 
     mapping (uint32 => Product) products;
@@ -28,11 +29,11 @@ contract ProductChain {
 
     }
 
-    function addProduct(string memory c_name, string memory c_origin, string memory pr_name, string memory pr_descr, uint32 pr_id) public {
+    function addProduct(bytes32 c_name, bytes32 c_origin, bytes32 pr_name, bytes32 pr_descr, uint32 pr_id) public {
 
         Company memory cmp;
         Transaction memory tr;
-        Product memory pr;
+        Product storage pr = products[pr_id];
 
         cmp.name = c_name;
         cmp.origin = c_origin;
@@ -41,25 +42,53 @@ contract ProductChain {
         pr.name = pr_name;
         pr.description = pr_descr;
         pr.id = pr_id;
+        pr.transactions_count = 1;
 
         tr.buyer = cmp;
         tr.datastamp = now;
         tr.id = 0;
 
-        products[pr_id] = pr;
         transactions[pr_id][0] = tr;
     }
 
-//     function buyProduct(uint32 pr_id, uint8 tr_id) public {
-//
-//         Product storage pr = products[pr_id];
-//         Company storage seller = pr.transactions[pr.transactions.length - 1].buyer;
-//         require(msg.sender == seller.p_key);
-//
-//         Transaction memory tr;
-//         tr.seller = seller;
-//         tr.id = tr_id;
-//
-//         pr.transactions[pr.transactions.length] = tr;
-//     }
+    function sellProduct(uint32 pr_id, uint32 tr_id) public {
+
+        uint32 tr_count = products[pr_id].transactions_count;
+        require(msg.sender == transactions[pr_id][tr_count - 1].buyer.p_key);
+
+        Transaction memory tr;
+        tr.seller = transactions[pr_id][tr_count - 1].buyer;
+        tr.id = tr_id;
+
+        transactions[pr_id][tr_count] = tr;
+        products[pr_id].transactions_count += 1;
+    }
+
+    function buyProduct(uint32 pr_id, uint32 tr_id, bytes32 s_name, bytes32 s_origin) public {
+
+        uint32 tr_count = products[pr_id].transactions_count;
+        require(tr_id == transactions[pr_id][tr_count - 1].id);
+
+        transactions[pr_id][tr_count - 1].buyer.name = s_name;
+        transactions[pr_id][tr_count - 1].buyer.p_key = msg.sender;
+        transactions[pr_id][tr_count - 1].buyer.origin = s_origin;
+    }
+
+    function getInformation(uint32 pr_id) public view
+        returns(bytes32 pr_name, bytes32 pr_descr, bytes32 producer, bytes32 origin, address p_key, bytes32[10] memory sell_names, bytes32[10] memory buy_names, address[10] memory sell_addr, address[10] memory buy_addr) {
+
+        Product storage pr = products[pr_id];
+
+        pr_name = pr.name;
+        pr_descr = pr.description;
+        producer = transactions[pr_id][0].buyer.name;
+        origin = transactions[pr_id][0].buyer.origin;
+        p_key = transactions[pr_id][0].buyer.p_key;
+        for (uint i = 0; i < pr.transactions_count; i++) {
+            sell_names[i] = transactions[pr_id][i].seller.name;
+            sell_addr[i] = transactions[pr_id][i].seller.p_key;
+            buy_names[i] = transactions[pr_id][i].buyer.name;
+            buy_addr[i] = transactions[pr_id][i].buyer.p_key;
+        }
+    }
 }
