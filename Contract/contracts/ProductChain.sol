74545679aad753cb427a7623601073dba8b10bf7
Samuil Georgiev <sami.georgiev@gmail.com>
2019-03-15 13:35:34 +0200

    Fixed bugs in addPr

--- Contract/contracts/ProductChain.sol
+++ Contract/contracts/ProductChain.sol
@@ -1,49 +1,65 @@
 pragma solidity ^0.5.0;
 
 contract ProductChain {
-    
+
     struct Company {
         string name;
         string origin;
         address p_key;
     }
-    
+
     struct Transaction {
         Company seller;
         Company buyer;
         uint datastamp;
+        uint8 id;
     }
-    
+
     struct Product {
         string name;
         string description;
-        int32 id;
-        Transaction[] transactions;
+        uint32 id;
     }
-    
-    mapping (int32 => Product) products;
-    
+
+    mapping (uint32 => Product) products;
+    mapping (uint32 => Transaction[]) transactions;
+
     constructor() public {
-      
+
     }
-    
-    function createProduct(string memory c_name, string memory c_origin, string memory pr_name, string memory pr_descr, int32 pr_id) public {
-        
+
+    function addProduct(string memory c_name, string memory c_origin, string memory pr_name, string memory pr_descr, uint32 pr_id) public {
+
         Company memory cmp;
         Transaction memory tr;
         Product memory pr;
-        
+
         cmp.name = c_name;
         cmp.origin = c_origin;
         cmp.p_key = msg.sender;
-        
-        tr.buyer = cmp;
-        tr.datastamp = now;
-        
+
         pr.name = pr_name;
         pr.description = pr_descr;
         pr.id = pr_id;
+
+        tr.buyer = cmp;
+        tr.datastamp = now;
+        tr.id = 0;
+
+        products[pr_id] = pr;
+        transactions[pr_id][0] = tr;
     }
-  
-  
+
+//     function buyProduct(uint32 pr_id, uint8 tr_id) public {
+//
+//         Product storage pr = products[pr_id];
+//         Company storage seller = pr.transactions[pr.transactions.length - 1].buyer;
+//         require(msg.sender == seller.p_key);
+//
+//         Transaction memory tr;
+//         tr.seller = seller;
+//         tr.id = tr_id;
+//
+//         pr.transactions[pr.transactions.length] = tr;
+//     }
 }
