97d00443fc59ec39f4cfd35392361b2ae122b5e2
Martin Jordanov <xxm0703@abv.bg>
2019-03-15 19:41:13 +0200

    Whole project restructuring

--- sellers/produce/vue.js
+++
@@ -1,9 +0,0 @@
-function composeJSON() {
-	let obj = {}
-	let form = document.getElementById('form');
-	obj['name'] = form[0].value;
-	obj['company'] = form[1].value;
-	obj['origin'] = form[2].value;
-    obj['description'] = form[3].value;
-	console.log(obj);
-}
