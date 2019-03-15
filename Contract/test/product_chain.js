f2c9fc49b5dddccb8cc02de53bb7a814bb9de5a1
Samuil Georgiev <sami.georgiev@gmail.com>
2019-03-15 13:44:54 +0200

    Migration added

---
+++ Contract/test/product_chain.js
@@ -0,0 +1,7 @@
+contract('ProductChain', function(accounts) {
+  it("should assert true", function(done) {
+    var product_chain = ProductChain.deployed();
+    assert.isTrue(true);
+    done();
+  });
+});
