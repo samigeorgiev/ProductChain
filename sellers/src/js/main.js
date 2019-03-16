bdb3acb3e17f4a5126de06dbc528431aa7e3ed0f
Martin Jordanov <xxm0703@abv.bg>
2019-03-16 09:21:50 +0200

    Transfer page restructuring

--- sellers/src/js/main.js
+++ sellers/src/js/main.js
@@ -1,10 +1,53 @@
+new Vue({
+	el: '#form',
+	data: {
+		notSubmitted: true,
+		scan: false,
+
+		company: "Company Name",
+		company_default: "Company Name",
+
+		productId: "Product ID",
+		productId_default: "Product ID",
+		passphrase: "Passphrase",
+		passphrase_default: "Passphrase",
+	},
+
+	methods: {
+		companyFocus: function() {
+			this.company = '';
+		},
+		productFocus: function() {
+			this.productId = '';
+		},
+		passphraseFocus: function() {
+			this.passphrase = '';
+		},
+
+		companyFocusOut: function() {
+			this.company = this.company_default;
+		},
+		productFocusOut: function() {
+			this.productId = this.productId_default;
+		},
+		passphraseFocusOut: function() {
+			this.passphrase = this.passphrase_default;
+		},
+
+		transfer: function() {
+			composeJSON();
+			composeQR();
+			notSubmitted = false;
+		}
+	}
+
+})
+
 function composeJSON() {
 	let obj = {}
-	let form = document.getElementById('form');
-	for (var i = 0; i < 3; i++) {
-		obj[form[i].id] = strToHex(form[i].value);
-	}
-	console.log(form.length);
+	let productId = document.getElementById('productId').value;
+
+	obj['productId'] = productId;
 	obj['transactionId'] = Math.floor(Math.random() * 256);
 
 	const passphrase = form[3].value;
