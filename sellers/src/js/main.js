c1b082e6f709c77434c6fed7f917a0aaf524ab45
Samuil Georgiev <sami.georgiev@gmail.com>
2019-03-16 09:43:00 +0200

    Merge branch 'master' of https://github.com/joro2404/ProductChain

--- sellers/src/js/main.js
+++ sellers/src/js/main.js
@@ -23,4 +23,19 @@ function qrCode() {
 	makeCode();
 }
 
-function strToHex(str) { return parseInt(str.replace(/^#/, ''), 16); }
+function strToHex(str) {
+	var hex = '';
+	for(var i=0;i<str.length;i++) {
+		hex += ''+str.charCodeAt(i).toString(16);
+	}
+	return hex;
+}
+
+function hexToStr(hex) {
+	hex  = hex.toString();
+	var str = '';
+	for (var n = 0; n < hex.length; n += 2) {
+		str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+	}
+	return str;
+}
