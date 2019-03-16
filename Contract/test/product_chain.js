c51a0c365e2a53ee9c0b92361c51faef338ed4bb
Samuil Georgiev <sami.georgiev@gmail.com>
2019-03-16 10:11:39 +0200

    Test doesnt work

--- Contract/test/product_chain.js
+++ Contract/test/product_chain.js
@@ -8,18 +8,30 @@ function strToHex(str) {
 	return '0x' + hex;
 }
 
+function hexToStr(hex) {
+	hex  = hex.toString();
+    hex = hex.split('').slice(2).join('');
+	var str = '';
+	for (var n = 0; n < hex.length; n += 2) {
+        var ch = String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+        if (ch == '0') { break; }
+        str += ch;
+	}
+	return str;
+}
+
 contract('ProductChain', function(accounts) {
   it('should create products', function() {
     return ProductChain.deployed().then(function(contract) {
-        return contract.addProduct('0x0122', strToHex('USA'), strToHex('SD Card'), strToHex('8GB'), 1234);
+        return contract.addProduct(strToHex('Kingston'), strToHex('USA'), strToHex('SD Card'), strToHex('8GB'), 1234, { from: accounts[0] });
     });
   });
 
-/*  it('should return info', function() {
+  it('should return info', function() {
       return ProductChain.deployed().then(function(contract) {
           return contract.getInformation(1234);
       }).then(function(result) {
-          assert.isTrue(result[0].toString(16) === 'Kingston');
+          assert.isTrue(hexToStr(result[0] === 'SD Card\\u0000\\u0000\\u0000\\u0000\\u0000\\u0000\\u0000\\u0000\\u0000\\u0000\\u0000\\u0000\\u0000\\u0000\\u0000\\u0000\\u0000\\u0000\\u0000\\u0000\\u0000\\u0000\\u0000\\u0000\\u0000'));
       });
-  }); */
+  });
 });
